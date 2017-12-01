1f47ff0ad54935511e37636a92d8a2aba30a93ad
Diamond <nataniel.r.v@gmail.com>
2017-12-01 17:41:01 -0200

    well, some shitty code to take a look at home...

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,57 @@
 import React, { Component } from 'react';
-import "./dist/App.css";
 import episode_list from './episode_list.json'
+import './App.css';
+import {DM} from './all.js'
 
 class App extends Component{
     constructor(props){
         super(props);        
+
         this.handleSeassonClick = this.handleSeassonClick.bind(this);
-        
+        this.randomTrue = this.randomTrue.bind(this);
+        this.playVideo = this.playVideo.bind(this);
+
         this.state ={
             seassons: episode_list,
-            current_seasson: ''
+            current_seasson: '',
+            generateRandom: false
         }
     }
 
-    handleSeassonClick(event){
-        this.setState({current_seasson: event.target.value});
+    playVideo(e){
+        var player = DM.player(document.getElementById(e.target.id), {
+            video: "xwr14q",
+            params: {
+                autoplay: true,
+                mute: true
+            }
+        });
     }
 
+    handleSeassonClick(event){             
+        this.setState({current_seasson: event.target.value, generateRandom: false});                           
+    }
+   
+    getRandomEpisode(){
+        var randomSeason = this.state.seassons[Math.floor(Math.random()*this.state.seassons.length)];
+        var randomEpisode = randomSeason.episodes[Math.floor(Math.random()*randomSeason.episodes.length)]        
 
-    render(){
+        return (
+            <div className="col-sm-3">
+                <div className={"card animate_".concat(this.state.toggle_animation)}>                            
+                    
+                    <iframe title={randomEpisode.name} className="card-img-top" src="" allowFullScreen frameBorder="0"></iframe>
+                    
+                    <div className="card-body">
+                        <p className="card-text text-center"><small>{randomEpisode.name}</small></p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 
-        const episodios = this.state.seassons
+    getEpisodes(){
+        return this.state.seassons
             .filter(seasson => {
                 return seasson.seasson_number === this.state.current_seasson;
             })
@@ -28,39 +59,73 @@ class App extends Component{
                 return seasson.episodes.map(episode =>{
                     return (
                         <div className="col-sm-3">
-                            <div className="card">                            
-                                <video className="card-img-top" controls="controls" src={episode.video_url} />
+                            <div className={"card animate_".concat(this.state.toggle_animation)}>                            
+                                
+                                <div id={episode.video_url} className="card-img-top"
+                                    onClick={this.playVideo}
+                                ></div>
+                                
+                                
                                 <div className="card-body">
-                                    <p className="card-text"><small>{episode.name}</small></p>
+                                    <p className="card-text text-center"><small>{episode.name}</small></p>
                                 </div>
                             </div>
                         </div>
                     )
                 })                
             })
+    }
 
+    randomTrue(){
+        this.setState({generateRandom: true})
+    }
+
+    render(){
+
+        
         return(
             <div className="row">
-                <div className="col-sm-3">
-                    <div className="list-group">
-                        {
+                <div className="col-sm-2">
+                    
+                    <button 
+                        type="button" 
+                        className="list-group-item list-group-item-action text-center"
+                        data-toggle="collapse"
+                        data-target="#list-seasons"
+                        aria-expanded="false" 
+                        aria-controls="list-seasons"
+                    >Seasons
+                        <span className="badge badge-pill badge-dark float-right">{this.state.seassons.length} { " seasons"}</span>
+                    </button>
+                
+                    <div id="list-seasons" className="collapse">
+                        <div className="list-group">
+                        {                            
                             this.state.seassons.map((seasson) =>
                                 <button                                    
-                                    className={"list-group-item list-group-item-action"}
+                                    className={"list-group-item list-group-item-action text-center "}
                                     id={"list-seasson-".concat(seasson.seasson_number).concat("-list") }                                    
                                     onClick={this.handleSeassonClick}
                                     value={seasson.seasson_number}
                                 >
                                     {"Season - ".concat(seasson.seasson_number)}
+                                    <span className="badge badge-pill badge-success float-right">{seasson.episodes.length} { " episodes"}</span>
                                 </button>  
-                            )
-                        }                            
+                            )                            
+                        }    
+                        </div>                        
                     </div>
+                    <button 
+                        type="button"
+                        className="list-group-item list-group-item-action text-center"
+                        onClick={this.randomTrue}
+                    >Random episode
+                    </button>
                 </div>
-                <div className="row col-sm-9">                    
-                        {                            
-                            episodios
-                        }
+                <div className="row col-sm-10">                    
+                    {                        
+                        this.state.generateRandom === false ? this.getEpisodes() : this.getRandomEpisode()
+                    }
                 </div>
             </div>
         );
