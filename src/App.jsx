822b04752b8460e0e05d881f198049375c57cff3
Diamond <nataniel.r.v@gmail.com>
2017-12-01 17:41:20 -0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import episode_list from './episode_list.json'
 import './App.css';
-import {DM} from './all.js'
 
 class App extends Component{
     constructor(props){
